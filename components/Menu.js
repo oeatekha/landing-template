18986e473ee93607badbc3e3b1fde6bd96e6bf32
Omoruyi Atekha <55259950+oeatekha@users.noreply.github.com>
2023-03-27 19:57:49 -0400

    remove background?

--- components/Menu.js
+++ components/Menu.js
@@ -7,7 +7,7 @@ export default function DropDown() {
     return (
 
             
-<nav className="bg-white sm:px-4 py-2 rounded">
+<nav className="sm:px-4 py-2 rounded">
     <div className="p-4 menu sticky top-0 flex items-center justify-between" >
             {/*Import pollen logo from public folder name is pollen_logo.svg*/}
             <div className="flex items-center">
