9ed55f4fb0702f9b9c0ea9caee02bd42272f10fa
Omoruyi Atekha <55259950+oeatekha@users.noreply.github.com>
2023-04-17 08:09:18 -0700

    pollen website

--- components/Menu.js
+++ components/Menu.js
@@ -7,25 +7,30 @@ export default function DropDown() {
     return (
 
             
-<nav className="sm:px-4 py-2 rounded">
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2 rounded">
     <div className="p-4 menu sticky top-0 flex items-center justify-between" >
             {/*Import pollen logo from public folder name is pollen_logo.svg*/}
             <div className="flex items-center">
                 <Link href="/">
                     <a className="flex items-center">
-               
-                        <span className="text-xl font-bold ml-2 text-gray-800">
-                            grdn
-                        </span>
+                        <img
+                            src="/pollen_logo.svg"
+                            alt="Pollen Logo"
+                            className="h-8 w-8"
+                        />
+
                     </a>
                 </Link>
                 
             </div>
             <Menu as="div" className="relative inline-block text-left menu-bg">
-                <div className="flex flex-row items-center">
-
-                    <Menu.Button className=" focus:outline-none inline-flex w-full justify-center rounded-md bg-green-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                <div>
+                    <Menu.Button className=" focus:outline-none inline-flex w-full justify-center rounded-md bg-yellow-400 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
                         Engineerd By Pollen
+                        <ChevronDownIcon
+                            className="ml-2 -mr-1 h-5 w-5"
+                            aria-hidden="true"
+                        />
                     </Menu.Button>
                 </div>
                 <Transition
@@ -39,14 +44,26 @@ export default function DropDown() {
                 >
                     <Menu.Items className="focus:outline-none absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 menuContent">
                         <div className="px-3 py-3 flex flex-col">
+
                             <Menu.Item>
                                 {({ active }) => (
-                                    <Link href="https://twitter.com/currantts"
-                                        className={`${active ? 'bg-en-blue-500 text-white' : 'text-gray-900'
+                                    <a href="https://twitter.com/pppollen"
+                                        className={`${active ? 'bg-neutral-300 text-white' : 'text-gray-900'
                                             } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                                     >
                                         Twitter
-                                    </Link>
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a href="mailto: admin@pollen.rs"
+                                        className={`${active ? 'bg-neutral-300 text-white' : 'text-gray-900'
+                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        Contact Us
+                                    </a>
                                 )}
                             </Menu.Item>
                         </div>
